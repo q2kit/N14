97997ea123235d480f8b6872e057b18ebc382378
nguyenphuctinh <phuctinh2001@gmail.com>
2021-10-27 21:56:35 +0700

    footer

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ from .models import Customer, Product
 
 def index(request):
     Data = {'products': Product.objects.all()}
-    return render(request, 'index.html')
+    return render(request, 'index.html', Data)
 
 
 def register(request):
