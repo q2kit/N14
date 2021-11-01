f9eaf0df4150faf33a88389ff29a8f57d2f9e1a7
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-01 15:16:37 +0700

    fe

--- Store/settings.py
+++ Store/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
