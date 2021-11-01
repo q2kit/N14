ef4bf1646a2feaa4d0647c8ff2565bd9335d6036
Doan Huu Quan <dhq1440p@gmail.com>
2021-11-01 20:36:21 +0700

    ..

--- home/models.py
+++ home/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     id = models.CharField(max_length=32, primary_key=True)
 
     def __str__(self):
-        return self.id
+        return self.name
 
 
 class Product_img(models.Model):
@@ -29,9 +29,16 @@ class Product_img(models.Model):
 
 
 class Order(models.Model):
+    id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    customer = models.CharField(max_length=10)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    stt = (
+        ('done', 'Đã hoàn thành'),
+        ('incart','Trong giỏ hàng'),
+        ('xxx', 'xxx')
+    )
+    status = models.CharField(max_length=20,choices=stt,default='inCart')
 
     def __str__(self):
         return self.product.name
