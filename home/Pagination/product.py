f75a059c99d59409e67e58724940f4f110593727
nguyenphuctinh <phuctinh2001@gmail.com>
2021-11-26 14:22:25 +0700

    page

--- home/Pagination/product.py
+++ home/Pagination/product.py
@@ -3,6 +3,5 @@ from django.views.generic import ListView
 from home.models import Product
 
 
-class ContactListView(ListView):
-    paginate_by = 2
+class ProductListView(ListView):
     model = Product
